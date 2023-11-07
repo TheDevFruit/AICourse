6844f57dedec0c63c6bb82a5218e67cc2d86271e
TheDevFruit <timofejfruit97737@gmail.com>
2023-11-07 13:06:46 +0300

    Third Update

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 # Git Repository: https://github.com/TheDevFruit/AICourse
+
+array = [[1.05, 2, "a"], ["cvb", 24.6, 7], ["Hello", "hi", 11]]
+
+out = []
+for i in array:
+    for j in i:
+        out.append(j)
+
+print(array)
+print(out)
