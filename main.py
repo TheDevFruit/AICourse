e2cf8aaacd730bba31f81492024b66d84ccde853
TheDevFruit <timofejfruit97737@gmail.com>
2023-11-09 14:41:11 +0300

    Third Update

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 # Git Repository: https://github.com/TheDevFruit/AICourse
 
-array = [[1.05, 2, "a"], ["cvb", 24.6, 7], ["Hello", "hi", 11]]
-
-out = []
-for i in array:
-    for j in i:
-        out.append(j)
-
-print(array)
-print(out)
+array = [1, 6, 2, 3, 8, 6, 4, 9, 10, 22, 0, -1]
+tuple_array = [(i, i**2) for i in array]
+print(tuple_array)
