466c3bd88cd23eb33f22a5930a44311c8a55ac98
TheDevFruit <timofejfruit97737@gmail.com>
2023-11-21 15:43:43 +0300

    Five Update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # Git Repository: https://github.com/TheDevFruit/AICourse
 
-array = [1, 6, 2, 3, 8, 6, 4, 9, 10, 22, 0, -1]
-tuple_array = [(i, i**2) for i in array]
-print(tuple_array)
+def first_letter(x):
+    return x[0]
 
+
+arr = ['Иван', 'Алиса', 'Петр', 'Ольга', 'Евгения', 'Дмитрий', 'Ли']
+arr.sort(key=first_letter)
+for i in range(len(arr)): print(f"{i} {arr[i]}")
