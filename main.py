34a64fd71c485897747f4e60b439625a44915035
TheDevFruit <timofejfruit97737@gmail.com>
2023-11-21 15:48:39 +0300

    Six Update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Git Repository: https://github.com/TheDevFruit/AICourse
 
-def first_letter(x):
-    return x[0]
-
-
-arr = ['Иван', 'Алиса', 'Петр', 'Ольга', 'Евгения', 'Дмитрий', 'Ли']
-arr.sort(key=first_letter)
-for i in range(len(arr)): print(f"{i} {arr[i]}")
+def match(x):
+    if x == "save":
+        return "сохранить"
+    elif x == "load":
+        return "загрузить"
+    else:
+        return "неизвестная операция"
