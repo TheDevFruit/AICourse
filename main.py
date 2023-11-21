0baa7e8fb8532a2b3251dfdcb99a11ad79231090
TheDevFruit <timofejfruit97737@gmail.com>
2023-11-21 15:56:38 +0300

    Six Update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Git Repository: https://github.com/TheDevFruit/AICourse
 
-def match(x):
-    if x == "save":
-        return "сохранить"
-    elif x == "load":
-        return "загрузить"
-    else:
-        return "неизвестная операция"
+num = int(input("Введите количество Рублей: "))
+if num // 10 != 1 and num % 10 == 1:
+    print(f"{num} рубль")
+elif num // 10 != 1 and (num % 10 == 2 or num % 10 == 3 or num % 10 == 4):
+    print(f"{num} рубля")
+else:
+    print(f"{num} рублей")
