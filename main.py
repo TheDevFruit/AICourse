44bcebfa520f340d56a80e468cb181bd49043801
TheDevFruit <timofejfruit97737@gmail.com>
2023-11-23 14:41:59 +0300

    Eight Update

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # Git Repository: https://github.com/TheDevFruit/AICourse
 
-num = int(input("Введите количество Рублей: "))
-if num // 10 != 1 and num % 10 == 1:
-    print(f"{num} рубль")
-elif num // 10 != 1 and (num % 10 == 2 or num % 10 == 3 or num % 10 == 4):
-    print(f"{num} рубля")
-else:
-    print(f"{num} рублей")
-
+arr = [0, 1]
+n = int(input("Введите N-Число: "))
+for i in arr:
+    arr.append(arr[len(arr)-1] + arr[len(arr)-2])
+    if len(arr)-1 == n:
+        print(arr[len(arr)-1])
+        break
