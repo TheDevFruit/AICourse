0877caa6f797896766277c7335a8e649d973e2fb
TheDevFruit <timofejfruit97737@gmail.com>
2023-11-23 15:17:05 +0300

    Eleven Update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Git Repository: https://github.com/TheDevFruit/AICourse
 
-text = str(input("Введите Текст: "))
-count = 0
-for i in range(len(text)):
-    if text[i].lower() == "к" and i < len(text)-2:
-         if text[i] + text[i+1] + text[i+2] in ["Кот", "КОТ", "кот"]:
-             count += 1
-print(count)
+arr = ['Иван', 'Алиса', 'Петр', 'Ольга', 'Евгения', 'Дмитрий', 'Ли']
+
+for_first = lambda x: x[0]
+arr.sort(key=for_first)
+
+for i in range(len(arr)):
+    print(f"{i} {arr[i]}")
